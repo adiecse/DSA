b3cf1783012162ca64548e8552cd0f6c96167f80
adi673 <codingadityawadgaonkar@gmail.com>
2023-03-28 01:30:04 +0530

    Q1]

--- Recursion/Q1]findmaxinArr.cpp
+++ Recursion/Q1]findmaxinArr.cpp
@@ -17,12 +17,33 @@ void findmax(int arr[], int n, int i, int &maxi) {
   findmax(arr, n, i + 1, maxi);
 }
 
+void findmin(int arr[], int n, int i, int &mini){
+   //base case
+   if(n<=i){
+    return ;
+   }
+
+   //solve case 
+   if(arr[i]<mini){
+    mini=arr[i];
+   }
+  //cout<<mini<<endl;
+
+   //recursion case
+   findmin(arr, n, i + 1, mini);
+
+}
+
 int main() {
   int arr[6] = {10, 14, 4, 22, 88, 42};
   int n = 6;
   int maxi = INT_MIN;
+  int mini = INT_MAX;
   findmax(arr, 6, 0, maxi);
+  findmin(arr, 6, 0, mini);
   int max = maxi;
-  cout << max << " " << endl;
-  cout << max << endl;
+  int min = mini;
+  cout <<"max is ->" << max << " " << endl;
+  cout <<"min is ->" <<min << " " << endl;
+  
 }
