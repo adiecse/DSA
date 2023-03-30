d1173368b2563bf5d12c428002c6486a36641fb9
adi673 <codingadityawadgaonkar@gmail.com>
2023-03-30 19:52:45 +0530

    Q7

--- Recursion/Q7]COinFlip.cpp
+++ Recursion/Q7]COinFlip.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <limits.h>
-#include <vector>
+#include<iostream>
+#include<vector>
+#include<limits.h>
 using namespace std;
 
-int mink(vector<int> &arr, int tar) {
-  int mini = INT_MAX;
-  if (tar == 0) {
+int MinFlip(vector<int>& arr,int tar){
+  int mini=INT_MAX;
+  if(tar==0){
     return 0;
   }
 
-  if (tar < 0) {
+  if(tar <0){
     return INT_MAX;
   }
 
-  for (int i = 0; i < arr.size(); i++) {
-    int ans = mink(arr, tar - arr[i]);
-    if (ans != INT_MAX) {
-      mini = min(ans + 1, mini);
+  for(int i=0; i<arr.size(); i++){
+    int ans=MinFlip(arr,tar-arr[i]);
+    if(ans!=INT_MAX){
+      mini=min(mini,ans+1);
     }
   }
   return mini;
+  
 }
 
-int main() {
-  vector<int> v = {1, 2};
-  int tar = 4;
-  int n = v.size();
-  int b = mink(v, tar);
-  cout << b << endl;
+int main(){
+  vector<int> v={1,2};
+  int target=5;
+  int ans=MinFlip(v,target);
+  cout<<ans<<":is"<<endl;
 }
